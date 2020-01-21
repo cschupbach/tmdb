5283f53c81baad2e69dd8fcc9e760b52cf130d40
Chad Schupbach <38390041+cschupbach@users.noreply.github.com>
2020-01-20 19:28:15 -0600

    Add B-splines basis matrix functions

--- src/visualization/utils/splines.py
+++ src/visualization/utils/splines.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 
-def M_splines_basis(x, M, K):
+def m_splines_basis(x, M, K):
+    """Return the basis matrix for order-M splines."""
     dof = K + M
     xi = np.linspace(np.min(x), np.max(x), K+2)[1:-1]
     H = np.zeros((len(x), dof))
@@ -13,8 +14,8 @@ def M_splines_basis(x, M, K):
     return H
 
 
-def M_splines(x, M=4, K=8):
-    H = splines_basis(x, M, K)
+def m_splines(x, M=4, K=8):
+    H = m_splines_basis(x, M, K)
     bhat = np.linalg.inv(H.T@H)@(H.T@y)
     yhat = H@bhat
 
@@ -31,3 +32,33 @@ def plot_mean_function(x, y, yhat):
     plt.show()
 
     return None
+
+
+def tau_knot_sequence(x, K, M):
+    a = np.min(x)
+    b = np.max(x)+1e-8
+    t_inner = np.linspace(a, b, K+2)[1:-1]
+    t_left = np.array([a]*M)
+    t_right = np.array([b]*M)
+    tau = np.append(t_left, t_inner)
+    tau = np.append(tau, t_right)
+    return tau
+
+
+def b_basis(x, t, m, j):
+    if m == 0:
+        return 1 if t[j] <= x < t[j+1] else 0
+    if t[j+m] == t[j]:
+        a = 0.0
+    else:
+        a = (x-t[j])/(t[j+m]-t[j]) * b_basis(x, t, m-1, j)
+    if t[j+m+1] == t[j+1]:
+        b = 0.0
+    else:
+        b = (t[j+m+1] - x)/(t[j+m+1] - t[j+1]) * b_basis(x, t, m-1, j+1)
+    return a + b
+
+
+def b_splines_basis(x, t, m):
+    n = len(t)-m
+    return [b_basis(x, t, m, j) for j in range(n-1)]
