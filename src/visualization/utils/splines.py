217b846e410dccefa0a469a353bccd331ba0700d
Chad Schupbach <38390041+cschupbach@users.noreply.github.com>
2020-01-22 19:10:05 -0600

    Add smoothing splines and Gaussian kernel regression

--- src/visualization/utils/splines.py
+++ src/visualization/utils/splines.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def m_splines_basis(x, M, K):
@@ -62,3 +63,67 @@ def b_basis(x, t, m, j):
 def b_splines_basis(x, t, m):
     n = len(t)-m
     return [b_basis(x, t, m, j) for j in range(n-1)]
+
+
+def differentiate(B):
+    dB = np.zeros(np.shape(B))
+    dB[1:,:] = B[1:,:]-B[:-1,:]
+    dB[0,:] = dB[1,:]
+    return dB
+
+
+def lambda_selection(B, y, lambdas):
+    B1 = differentiate(B)
+    B2 = differentiate(B1)
+    RSS = np.zeros(len(lambdas))
+    dof = np.zeros(len(lambdas))
+    for i in range(len(lambdas)):
+        S = B@np.linalg.inv(B.T@B + lambdas[i]*(B2.T@B2))@B.T
+        yhat = S@y
+        RSS[i] = np.sum(np.power(yhat-y,2))
+        dof[i] = np.trace(S.dot(S))
+    return RSS, dof
+
+
+def plot_lambda_curves(B, y):
+    fig = plt.figure(figsize=(16,4))
+    ax = [plt.subplot(1,3,i+1) for i in range(3)]
+    n = 100
+    min_lam, max_lam = 0, 1
+    for i in range(len(ax)):
+        lambdas = np.linspace(min_lam, max_lam, n)
+        RSS, dof = lambda_selection(B, y, lambdas)
+        GCV = (RSS/n) / np.power((1 - (dof/n)), 2)
+        lamhat = lambdas[np.argmin(GCV)]
+        ax[i].ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+        ax[i].plot(lambdas, GCV)
+        ax[i].scatter(lamhat, GCV[np.argmin(GCV)], c='r', s=20, label=r'$\hat{\lambda}$')
+        if i == 0:
+            ax[i].set_ylabel(r'GCV($\lambda$)')
+        ax[i].set_xlabel(r'$\lambda$')
+        ax[i].axvline(lamhat, c='k', ls='--', lw=1, alpha=0.8)
+        ax[i].text(lamhat+(0.015*(10**-i)), np.max(GCV)-(np.max(GCV)-np.min(GCV))*0.05, '$\hat{\lambda}$')
+        min_lam = lamhat-(0.05*(10**-i))
+        max_lam = lamhat+(0.05*(10**-i))
+    return lamhat
+
+
+def gaussian_kernel(u, d=1):
+    return np.power(2*np.pi,-d/2) * np.exp(-np.power(u,2))
+
+
+def loocv_gaus_kernel_regression(x, y, lambdas):
+    n = len(x)
+    m = len(lambdas)
+    mse = np.zeros(m)
+    for j in range(m):
+        Lambda = lambdas[j]
+        err = np.zeros(n)
+        for i in range(n):
+            x_ = np.delete(x,i)
+            y_ = np.delete(y,i)
+            z = gaussian_kernel((x[i]-x_)/Lambda)
+            yke = np.sum(z*y_)/np.sum(z)
+            err[i] = y[i]-yke
+        mse[j] = np.mean(np.power(err,2))
+    return mse
